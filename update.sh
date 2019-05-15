4d4d25428f7962d8de404d0e29f669e1accee77d
Rinshad K Asharaf <40381766+rinshadka@users.noreply.github.com>
2019-05-15 10:54:24 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 export CCF_PATH=/opt/event-navigator-containers
 cd $CCF_PATH
-git pull
+sudo git pull
 docker image rm --force $(docker image ls  |grep none |awk '{print $3}')
 cd $CCF_PATH/cf-hasura 
-ccfmake stop 
-ccfmake start
+sudo ccfmake stop 
+sudo ccfmake start
 cd $CCF_PATH/cf-ui
-ccfmake stop 
-ccfmake build
-ccfmake start
+sudo ccfmake stop 
+sudo ccfmake build
+sudo ccfmake start
